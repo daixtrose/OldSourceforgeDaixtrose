b8b7b7c307d7f1e1e853596beed785d6ea6d4878
sukram <daixtrose@users.noreply.github.com>
2003-02-28 16:52:02 +0000

    fixed typo in copyright notice

--- daixtrose/daixtrose/src/linalg/Inverse.h
+++ daixtrose/daixtrose/src/linalg/Inverse.h
@@ -2,7 +2,7 @@
 //
 // Copyright (C) 2003 Markus Werle
 //
-// This file is part of the Daixt C++ Library.  This library is free
+// This file is part of the Daixtrose C++ Library.  This library is free
 // software; you can redistribute it and/or modify it under the
 // terms of the GNU Lesser General Public License as published by the
 // Free Software Foundation; either version 2.1, or (at your option)
