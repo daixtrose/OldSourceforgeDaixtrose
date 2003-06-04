6ecdc615edeea5cee72ef1dbd282758174d1f155
sukram <daixtrose@users.noreply.github.com>
2003-06-04 09:58:35 +0000

    mod. a using declarative to make gcc-3.4 happy

--- daixtrose/daixtrose/src/linalg/RowAndColumExtractors.h
+++ daixtrose/daixtrose/src/linalg/RowAndColumExtractors.h
@@ -610,8 +610,8 @@ OperatorDelimImpl<RowExtractor<VectorExpression<T> >,
     NumT LHS_Result = RowExtractor<VectorExpression<T> >(i)(arg.lhs());
     NumT RHS_Result = RowExtractor<VectorExpression<T> >(i)(arg.rhs());
 
-    using namespace Daixt::DefaultOps;
-//     using Daixt::DefaultOps::operator+;
+    //using namespace Daixt::DefaultOps;
+    using Daixt::DefaultOps::operator+;
 
     NumT Result = LHS_Result + RHS_Result;
     return Result;
--- daixtrose/daixtrose/src/linalg/Vector.h
+++ daixtrose/daixtrose/src/linalg/Vector.h
@@ -311,8 +311,8 @@ operator+=(const OtherT& Other)
 
   if (Daixt::CountOccurrence(Other, *this)) 
     {
-      using namespace Daixt::DefaultOps;
-      //using Daixt::DefaultOps::operator+; 
+      //using namespace Daixt::DefaultOps;
+      using Daixt::DefaultOps::operator+; 
       *this = *this + Other; // delegate to operator= which uses temporary
     }
   else
